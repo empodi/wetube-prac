db9cd65ad079a06e3ba973f5794effd7c5a48dac
seonghwan <kglein9513@gmail.com>
2022-01-01 14:34:34 +0900

    create server

---
+++ src/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🎸`);
+
+app.listen(PORT, handleListening/** event handling function */);
