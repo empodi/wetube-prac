698191acff3a2b6fa563128aefcec9efa22bdbe8
seonghwan <kglein9513@gmail.com>
2022-01-01 16:10:16 +0900

    middleware logger

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
+const logger = morgan("dev");
 
 const app = express();
 
+const handleHome = (req, res) => {
+    //console.log(res);
+    return res.end();
+}
+
+app.use(logger);
+app.get("/", handleHome);
+
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🎸`);
 
-app.listen(PORT, handleListening/** event handling function */);
+app.listen(PORT /** port # */, handleListening/** event handling function */);
