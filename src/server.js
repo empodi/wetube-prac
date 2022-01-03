173384c4e450622dfe6d54e1c7f757cad1b930a5
seonghwan <kglein9513@gmail.com>
2022-01-03 20:18:51 +0900

    start session

--- src/server.js
+++ src/server.js
@@ -13,6 +13,18 @@ app.use(logger);
 app.set("view engine", "pug"); // no need for import
 app.set("views", `${process.cwd()}/src/views`);
 app.use(express.urlencoded({ extended: true })); // translates HTML form into javascript object (POST - req.body)
+
+app.use(
+  // should be located before Routers
+  session({
+    // this middleware will remember everybody who comes to the website
+    // even the not logged in ones
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
