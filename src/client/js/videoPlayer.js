8078325acd7e958209e8ff2b96936a6854c42c99
empodi <kglein9513@gmail.com>
2022-01-27 23:08:04 +0900

    video mute/unmute volume

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,7 +2,10 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const time = document.getElementById("time");
 const muteBtn = document.getElementById("mute");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
+
+let currentVolume = 0.5;
+video.volume = currentVolume;
 
 const handlePlayClick = (event) => {
   if (video.paused) {
@@ -14,9 +17,30 @@ const handlePlayClick = (event) => {
 const handlePause = (event) => (playBtn.innerText = "Play");
 const handlePlay = (event) => (playBtn.innerText = "Pause");
 
-const handleMute = (event) => {};
+const handleMute = (event) => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : currentVolume;
+};
+
+const handleVolumeChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  currentVolume = value;
+  video.volume = value;
+};
 
 playBtn.addEventListener("click", handlePlayClick);
-muteBtn.addEventListener("mute", handleMute);
+muteBtn.addEventListener("click", handleMute);
 video.addEventListener("pause", handlePause);
 video.addEventListener("play", handlePlay);
+volumeRange.addEventListener("input", handleVolumeChange);
