8afcef947b2f15cff2b68113246b0026a45354c5
empodi <kglein9513@gmail.com>
2022-02-12 01:16:51 +0900

    Updated Dark & Light mode!!!!!!!

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -3,24 +3,67 @@ const form = document.getElementById("commentForm");
 const deleteBtn = document.querySelectorAll("#delete__comment");
 
 const addRealTimeComment = (text, id) => {
-  const videoComments = document.querySelector(".video__comments ul");
+  const videoComments = document.querySelector(".video__comments-container ul");
   const newComment = document.createElement("li");
+  const comment__container = document.getElementsByClassName(
+    "video__comments-container"
+  );
   newComment.dataset.comment_id = id;
   newComment.className = "video__comment";
 
-  const icon = document.createElement("i");
-  icon.className = "fas fa-comment";
-  const span = document.createElement("span");
-  span.innerText = ` ${text}`;
+  const user = comment__container[0].dataset.loggedinuser;
+  userObj = JSON.parse(user);
+  console.log(userObj);
+
+  const commentBox = document.createElement("div");
+  commentBox.className = "comment__box";
+
+  const commentOwner = document.createElement("div");
+  commentOwner.className = "comment__owner";
+
+  const commentDate = document.createElement("span");
+  commentDate.innerText = String(
+    new Date().toLocaleDateString({
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    })
+  );
+  commentDate.className = "comment__created-at";
+
+  const userName = document.createElement("span");
+  userName.innerText = userObj.username;
+  userName.className = "comment__user-name";
+  if (userObj.avatarUrl !== "") {
+    const userImg = document.createElement("img");
+    userImg.setAttribute("src", `/${userObj.avatarUrl}`);
+    const userName = document.createElement("span");
+    userName.innerText = userObj.username;
+    commentOwner.appendChild(userImg);
+  } else {
+    const userImg = document.createElement("span");
+    userImg.innerText = "😀";
+    commentOwner.appendChild(userImg);
+  }
+  commentOwner.appendChild(userName);
+  commentOwner.appendChild(commentDate);
+
+  const textContent = document.createElement("span");
+  textContent.innerText = ` ${text}`;
+
+  const commentContent = document.createElement("div");
+  commentContent.className = "comment__content";
+  commentContent.appendChild(textContent);
 
   const delButton = document.createElement("button");
   delButton.id = "delete__comment";
   delButton.dataset.comment_id = id;
-  delButton.innerText = "❌";
+  delButton.innerText = "Delete Comment";
   delButton.addEventListener("click", handleDeleteComment);
 
-  newComment.appendChild(icon);
-  newComment.appendChild(span);
+  newComment.appendChild(commentOwner);
+  newComment.appendChild(commentContent);
   newComment.appendChild(delButton);
   videoComments.prepend(newComment);
 };
@@ -68,7 +111,9 @@ const handleDeleteComment = async (event) => {
   const { status } = response;
 
   if (status === 201) {
-    const commentContainer = document.querySelector(".video__comments ul");
+    const commentContainer = document.querySelector(
+      ".video__comments-container ul"
+    );
 
     const commentArray = commentContainer.childNodes;
 
