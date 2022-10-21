f43889b8231382f7c727c89a942cfca3f8c0c382
empodi <kglein9513@gmail.com>
2022-10-21 22:07:42 +0900

    update

--- src/server.js
+++ src/server.js
@@ -31,7 +31,7 @@ app.use(
   // should be located before Routers
   session({
     // this middleware will remember everybody who comes to the website
-    // even not logged-in ones
+    // even not-logged-in ones
     secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false, // gives cookie only to logged in users
